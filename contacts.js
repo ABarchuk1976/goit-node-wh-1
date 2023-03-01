bff49ab345b6ebd52967b4842624eed182867c9c
Barchuk Andrii <abarchuk1976@gmail.com>
2023-03-01 16:59:25 +0200

    fixed all

---
+++ contacts.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const fs = require('fs').promises;
+
+const shortid = require('shortid');
+
+const contactsPath = path.join('./db/contacts.json');
+
+const listContacts = async () => {
+  try {
+    const bufferData = await fs.readFile(contactsPath);
+
+    const contacts = JSON.parse(bufferData);
+    console.log(contacts);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getContactByID = async (contactID) => {
+  try {
+    const bufferData = await fs.readFile(contactsPath);
+
+    const contact = JSON.parse(bufferData).find(
+      (item) => item.id === String(contactID)
+    );
+
+    console.log(contact);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const removeContact = async (contactID) => {
+  try {
+    const bufferData = await fs.readFile(contactsPath);
+
+    const contactsNew = JSON.parse(bufferData).filter(
+      (item) => item.id !== String(contactID)
+    );
+
+    await fs.writeFile(contactsPath, JSON.stringify(contactsNew));
+
+    console.log(contactsNew);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const addContact = async (contact) => {
+  try {
+    const contactNew = { id: shortid.generate(), ...contact };
+
+    const bufferData = await fs.readFile(contactsPath);
+
+    const contactsNew = [contactNew, ...JSON.parse(bufferData)];
+
+    await fs.writeFile(contactsPath, JSON.stringify(contactsNew));
+
+    console.log(contactsNew);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { listContacts, getContactByID, removeContact, addContact };
